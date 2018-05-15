cae14729cbd619db3ad7c2b855662c4c333848c1
Hassashin <danuamirudin@outlook.com>
2018-05-15 23:00:09 +0700

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Habibti :)" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Habibti" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Habibti :)\");
+            fail("\"message\" is not \"Hello Habibti\"");
         }
     }
 
